9c3048450e7edf3de38272bc3be0de22ee86f84d
Elaman Imashov <sosgames75743@gmail.com>
2021-02-24 01:34:20 +0600

    Home work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Sum(props) {
+ 
+ return <div className="Component">
+   <div className="Sum">Sum = { props.a + props.b }</div>
+ </div>;
+}
+
+function RandomNumber(props) {
+ 
+ return <div className="Component">
+   <div className="Rand">Random number = { props.min + (Math.round(Math.random() * props.max)) }</div>
+ </div>;
+}
+
+function BiggerNum(props) {
+ function Bigger (){
+   if(props.a > props.b){
+     return props.a;
+   }
+   else{
+     return props.b;
+   }
+ }
+ return <div className="Component">
+   <div className="Rand">Bigger number = { Bigger() }</div>
+ </div>;
+}
+
+function LiseItems(props) {
+ 
+  function addSpace(){
+    let res = "";
+    for (const i of props.list) {
+      res += i;
+      res += " ";
+    }
+    
+    console.log(res);
+    return res;
+  }
+
+ return <div className="Component">
+   <div className="Rand">List = { addSpace() }</div>
+ </div>;
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Sum a={5} b={4}></Sum>
+   <RandomNumber min={100} max={5000}></RandomNumber>
+   <BiggerNum a={100} b={1}></BiggerNum>
+   <LiseItems list = {['Apple', 'Pear', 'Limon', 'Tomato']}></LiseItems>
     </div>
   );
 }
